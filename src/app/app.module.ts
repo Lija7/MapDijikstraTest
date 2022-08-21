3f2ec290c2e6090742a3fdd719aa3ae059a9baf1
Pavle <pavle.lukic4@gmail>
2022-08-21 18:00:05 +0200

    dodavanje maplibre biblioteke i prikaz defaultne mape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
-
+import { NgxMapLibreGLModule } from '@maplibre/ngx-maplibre-gl';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +17,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatMenuModule,
-    MatButtonModule
+    MatButtonModule,
+    NgxMapLibreGLModule
   ],
   providers: [],
   bootstrap: [AppComponent]
